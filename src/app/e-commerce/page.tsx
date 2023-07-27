4f1f70a7feee7308f6a5e90a03964bdb416e8b2b
jsbadz <johnstephen.sb@gmail.com>
2023-07-27 15:43:24 +0800

    set-up

---
+++ src/app/e-commerce/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="lg:flex mb-32 text-center md:flex-none lg:mb-0 lg:text-left">
+                e-Commerce main page
+            </div>
+        </main>
+    )
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="lg:flex mb-32 text-center md:flex-none lg:mb-0 lg:text-left">
         <a
-          href=""
+          href="e-commerce"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          // target="_blank"
           rel="noopener noreaferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -21,9 +21,9 @@ export default function Home() {
         </a>
 
         <a
-          href=""
+          href="/portfolio"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          // target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
