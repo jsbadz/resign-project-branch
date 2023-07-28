76d316b96e39db27b521a9ad125a91a5c5052bb6
jsbadz <johnstephen.sb@gmail.com>
2023-07-28 10:07:39 +0800

    fix: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,39 @@
+import Link from "../../node_modules/next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="lg:flex mb-32 text-center md:flex-none lg:mb-0 lg:text-left gap-4">
-        <div className="">
-          <a
-            href="e-commerce"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-            // target="_blank"
-            rel="noopener noreaferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              e-Commerce{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about e-Commerce features and
-              resign-project.
-            </p>
-          </a>
-
-          <a
-            href="/portfolio"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-            // target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-               Admin
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-          </a>
+        <div className="w-[160px] font-bold text-black bg-gray-50 shadow-lg shadow-sky-400/50 p-3 rounded-md  hover:animate-bounce hover:bg-sky-100  animate-none ">
+          <div className="px-2 py-3">
+            <button className="hover:text-sky-500">
+              <Link className="flex" href="/e-commerce">
+                e-Commerce{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </Link>
+            </button>
+          </div>
+          <div className="px-2 py-3">
+            <button className="hover:text-sky-400">Admin</button>
+          </div>
         </div>
-        <div className="">
-          <a
-            href="e-commerce"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-            // target="_blank"
-            rel="noopener noreaferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Portfolio{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about Portfolio features and
-              resign-prodect.
-            </p>
-          </a>
 
-          <a
-            href="e-commerce"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-            // target="_blank"
-            rel="noopener noreaferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Admin
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-          </a>
+        <div className="w-[160px] font-bold text-black bg-gray-50 shadow-lg shadow-pink-400/50 p-3 rounded-md hover:animate-bounce hover:bg-pink-100 animate-none ">
+          <div className="px-2 py-3">
+            <button className="hover:text-pink-400">
+              <Link className="flex" href="/e-commerce">
+                Portfolio
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </Link>
+            </button>
+          </div>
+          <div className="px-2 py-3">
+            <button className="hover:text-pink-400">Admin</button>
+          </div>
         </div>
       </div>
     </main>
