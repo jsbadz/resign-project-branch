8fee28697d97bcfe576505005e1639d918e640f2
vince13 <vince.quinaging@gmail.com>
2023-07-28 17:01:20 +0800

    cart

--- src/app/_layout/Menu.tsx
+++ src/app/_layout/Menu.tsx
@@ -6,6 +6,9 @@ import { usePathname } from '../../../node_modules/next/navigation'
 import Link from '../../../node_modules/next/link'
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
+import Cart from '../component/Cart/Cart';
+import Image from '../../../node_modules/next/image';
+import { BiTrash } from "react-icons/bi";
 
 export const Menu = () => {
 
@@ -44,6 +47,33 @@ export const Menu = () => {
       },
    ]
 
+   const cartData = [
+      {
+         image: "../img/cart_p01.jpg",
+         name: "John Stephen Badelic",
+         quantity: "1",
+         price: 231,
+      },
+      {
+         image: "../img/cart_p02.jpg",
+         name: "Marichu Mae Niere",
+         quantity: "1",
+         price: 221,
+      },
+      {
+         image: "../img/cart_p01.jpg",
+         name: "Clara Mae Tacatani",
+         quantity: "1",
+         price: 131,
+      },
+      {
+         image: "../img/cart_p02.jpg",
+         name: "Sarah Mae Anga",
+         quantity: "1",
+         price: 431,
+      }
+   ]
+
    return (
       <>
          <div className="md:flex justify-between items-center text-md sticky top-0 z-20 transition-down bg-white">
@@ -59,7 +89,7 @@ export const Menu = () => {
                               <li key={index}>
                                  <Link
                                     href={item.url}
-                                    className={`cursor-pointer ${pathname === item.url ? "bg-[#f04336] text-white test px-5 py-2 font-[700] custom-rounded" : "text-[#1e4b57] hover:test hover:text-white hover:bg-[#f04336] font-[700] w-[200px] custom-rounded px-5 py-2"}`}
+                                    className={`nav-link cursor-pointer ${pathname === item.url ? "bg-[#f04336] text-white active px-5 py-2 font-[700] custom-rounded" : "text-[#1e4b57]  hover:text-white hover:bg-[#f04336] font-[700] w-[200px] custom-rounded px-5 py-2"}`}
                                  >
                                     {item.title}
                                  </Link>
@@ -70,9 +100,79 @@ export const Menu = () => {
                   </div>
                   <div className="flex items-center gap-5">
                      <BsSearch size={24} />
-                     <AiOutlineShoppingCart size={26}  />
+                     <Cart className=" relative" count={7} onClick={() => setShowCart(!showCart)} />
                      <h1 className="bg-[#f04336] p-2 rounded-lg text-white">ADOPT HERE TESTs</h1>
                   </div>
+                  {showCart ? (
+                     <div className={(showCart ? "right-0" : "right-0") + " bg-white top-[5rem] w-full px-5 sm:w-[380px] min-h-[400px] transition-right shadow-xl pb-5 z-40 absolute"}>
+                        {cartData && cartData.length ? (
+                           <div className="flex flex-col py-5 custom-scroll">
+                              <div className='overflow-y-auto h-[230px]'>
+                                 {cartData.map((items, i) => (
+                                    <div
+                                       className="flex flex-col justify-between"
+                                       key={i}
+                                    >
+                                       <div className="flex justify-between items-center gap-2">
+                                          <div className="flex">
+                                             <Image
+                                                loader={() =>
+                                                   "../img/cart_p01.jpg"
+                                                }
+                                                src={
+                                                   "../img/cart_p01.jpg"
+                                                }
+                                                width="100"
+                                                height="100"
+                                                objectFit="fill"
+                                                alt="image"
+                                                className="rounded-t-xl cursor-pointer"
+                                             />
+                                             <div className="flex flex-col justify-between">
+                                                <span className="text-lg font-[700]">
+                                                   {items.name}
+                                                </span>
+                                                <span className="text-md text-gray-500 font-[700]">
+                                                   100
+                                                </span>
+                                             </div>
+                                          </div>
+                                          <BiTrash size={24} />
+                                       </div>
+                                    </div>
+                                 ))}
+                              </div>
+                           </div>
+                        ) : (
+                           <>
+                              <div className="flex flex-col rounded-xl h-[230px] ">
+                                 <div className="flex flex-col justify-center items-center h-full">
+                                    <span className="p-3 text-default-purple">
+                                       <AiOutlineShoppingCart size={32} />
+                                    </span>
+                                    <label className="font-custom ">
+                                       Your cart is currently empty
+                                    </label>
+                                 </div>
+                              </div>
+                           </>
+                        )}
+                        <div className="flex justify-between mt-5">
+                           <span className="text-[#8e8e8e] text-md">
+                              Total:
+                           </span>
+                           <span className="text-[#8e8e8e] text-md">
+                              $180
+                           </span>
+                        </div>
+                        <button className="bg-[#f04336] text-white p-5 w-full uppercase text-sm mt-5">
+                           Shopping Cart
+                        </button>
+                        <button className="bg-[#0a303a] text-white p-5 w-full uppercase text-sm mt-2">
+                            Checkout
+                        </button>
+                     </div>
+                  ) : null}
                </div>
             </div>
             <div className="img-header left-0 w-full h-16 bg-repeat bg-center absolute bottom-[-20px] -z-20"></div>
---
+++ src/app/component/Cart/Cart.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+const Cart = (props) => {
+   const { count, className, onClick } = props
+   return (
+      <>
+         <div
+            className="flex justify-center items-center rounded-full text-black relative p-2 bg-default-purple cursor-pointer"
+            onClick={() => onClick()}
+         >
+            <AiOutlineShoppingCart size={26} />
+            <div className="flex justify-center items-center rounded-full text-black w-6 h-6 bg-default-yellow text-xs absolute -top-2 -right-3 font-semibold">
+               {count <= 9 ? count : "9+"}
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default Cart
